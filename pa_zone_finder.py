43c3dbb27e3d40e076829c27afc5bab8339d020a
Rosen Dimitrov <dimitrov.rosen@gmail.com>
2024-01-03 15:57:44 +0200

    fixed sap mapping

--- pa_zone_finder.py
+++ pa_zone_finder.py
@@ -10,12 +10,14 @@ pa_networks_list = []
 jump_connection = connect_to_jump_server()
 evi_connection = connect_to_evi(jump_connection)
 
-# sending commands to EVI
+
 with open('ip_list.txt', 'r') as f:
+    """ Reading IP list file """
     ip_list = f.read().splitlines()
     print('Collecting routes from EVI')
 
     for ip in ip_list:
+        """ Sending commands for each IP Route """
         print('!', end="")
         ip_mask = ip.split('/')
         ip, host_mask = ip_mask[0],  ip_mask[1],
@@ -26,17 +28,17 @@ with open('ip_list.txt', 'r') as f:
         interface = output.splitlines()[-1].split()[5]
 
         if interface in pa_fw_mapping:
-            network = Network(ip, host_mask, network, net_mask, pa_fw_mapping[interface])
-            pa_networks_list.append(network)
+            network_obj = Network(ip, host_mask, network, net_mask, pa_fw_mapping[interface])
+            pa_networks_list.append(network_obj)
 
         elif interface in sap_fw_mapping:
-            if not any(obj.network == network for obj in sap_networks_list):
-                network = Network(ip, host_mask, network, net_mask, pa_fw_mapping[interface])
-                sap_networks_list.append(network)
+            # if not any(obj.network == network for obj in sap_networks_list):
+            network_obj = Network(ip, host_mask, network, net_mask, zone=sap_fw_mapping[interface])
+            sap_networks_list.append(network_obj)
 
         else:
-            network = Network(ip, host_mask, network, net_mask, zone='BCN')
-            bcn_networks_list.append(network)
+            network_obj = Network(ip, host_mask, network, net_mask, zone='BCN')
+            bcn_networks_list.append(network_obj)
     print('')
 
 disconnect_from_evi(evi_connection)
@@ -48,8 +50,9 @@ pa_connection = connect_to_palo_alto(jump_connection)
 print('Collecting info from PA')
 
 for net in pa_networks_list:
+    """ Finding zones """
     command = f'test routing fib-lookup virtual-router {net.fw_name} ip {net.ip}'
-    print(command)
+    # print(command)
     output = send_commands_to_pa(pa_connection, command, r"---------", wait=2)
     if 'via' in output:
         interface = output.strip().splitlines()[6].split()[3][:-1]
@@ -57,7 +60,7 @@ for net in pa_networks_list:
         interface = output.strip().splitlines()[6].split()[1][:-1]
 
     zone_command = f"show interface {interface} | match Zone:"
-    print(zone_command)
+    # print(zone_command)
     zone_output = send_commands_to_pa(pa_connection, zone_command, string=r"virtual system:", wait=0.2)
     zone = zone_output.split()[1]
     net.interface = interface
@@ -71,13 +74,11 @@ sorted_pa_networks_list = sorted(pa_networks_list, key=lambda x: x.network)
 for net in sorted_pa_networks_list:
     print(net)
 
-print()
 sorted_bcn_networks_list = sorted(bcn_networks_list, key=lambda x: x.network)
 
 for net in sorted_bcn_networks_list:
     print(f"{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone}")
 
-print()
 sorted_sap_networks_list = sorted(sap_networks_list, key=lambda x: x.network)
 
 for net in sorted_sap_networks_list:
