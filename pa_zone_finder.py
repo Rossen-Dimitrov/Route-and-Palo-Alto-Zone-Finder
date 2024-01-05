c333daf6481d94cc0fda0bf1ba43dfeb7e72886b
Rosen Dimitrov <dimitrov.rosen@gmail.com>
2024-01-05 16:30:36 +0200

    added directly conneceted

--- pa_zone_finder.py
+++ pa_zone_finder.py
@@ -4,8 +4,10 @@ from router_connection import *
 from firewall_connection import *
 
 bcn_networks_list = []
+bcn_directly_connected_list = []
 sap_networks_list = []
 pa_networks_list = []
+vsx_firewall_list = []
 
 jump_connection = connect_to_jump_server()
 evi_connection = connect_to_evi(jump_connection)
@@ -22,10 +24,11 @@ with open('ip_list.txt', 'r') as f:
         ip_mask = ip.split('/')
         ip, host_mask = ip_mask[0],  ip_mask[1],
         command = f'display ip routing-table vpn-instance bcn-core {ip}'
-        output = send_commands_to_evi(evi_connection, command)
-        net = output.splitlines()[-1].split()[0]
+        output = send_commands_to_evi(evi_connection, command).splitlines()
+        net = output[-1].split()[0]
         network, net_mask = net.split('/')
-        interface = output.splitlines()[-1].split()[5]
+        interface = output[-1].split()[5]
+        directly_connected = output[-1].split()[1]
 
         if interface in pa_fw_mapping:
             network_obj = Network(ip, host_mask, network, net_mask, pa_fw_mapping[interface])
@@ -36,6 +39,9 @@ with open('ip_list.txt', 'r') as f:
             network_obj = Network(ip, host_mask, network, net_mask, zone=sap_fw_mapping[interface])
             sap_networks_list.append(network_obj)
 
+        elif directly_connected == "Direct":
+            network_obj = Network(ip, host_mask, network, net_mask, zone="BCN", interface='Directly Connected')
+            vsx_firewall_list.append(network_obj)
         else:
             network_obj = Network(ip, host_mask, network, net_mask, zone='BCN')
             bcn_networks_list.append(network_obj)
@@ -43,49 +49,66 @@ with open('ip_list.txt', 'r') as f:
 
 disconnect_from_evi(evi_connection)
 disconnect_from_jump_server(jump_connection)
-jump_connection = connect_to_jump_server()
 
-# sending commands to PA
-pa_connection = connect_to_palo_alto(jump_connection)
-print('Collecting info from PA')
 
-for net in pa_networks_list:
-    """ Finding zones """
-    command = f'test routing fib-lookup virtual-router {net.fw_name} ip {net.ip}'
-    # print(command)
-    output = send_commands_to_pa(pa_connection, command, r"---------", wait=2)
-    if 'via' in output:
-        interface = output.strip().splitlines()[6].split()[3][:-1]
-    else:
-        interface = output.strip().splitlines()[6].split()[1][:-1]
+# sending commands to PA if objects in pa_networks_list
+if pa_networks_list:
+    jump_connection = connect_to_jump_server()
+    pa_connection = connect_to_palo_alto(jump_connection)
+    print('Collecting info from PA')
 
-    zone_command = f"show interface {interface} | match Zone:"
-    # print(zone_command)
-    zone_output = send_commands_to_pa(pa_connection, zone_command, string=r"virtual system:", wait=0.2)
-    zone = zone_output.split()[1]
-    net.interface = interface
-    net.zone = zone
+    for net in pa_networks_list:
+        """ Finding zones """
+        command = f'test routing fib-lookup virtual-router {net.fw_name} ip {net.ip}'
 
-print()
-print('#' * 50, end='\n')
+        output = send_commands_to_pa(pa_connection, command, r"---------", wait=2)
+        if 'via' in output:
+            interface = output.strip().splitlines()[6].split()[3][:-1]
+        elif 'to host' in output:
+            interface = output.strip().splitlines()[6].split()[3]
+        else:
+            interface = output.strip().splitlines()[6].split()[1][:-1]
 
-sorted_pa_networks_list = sorted(pa_networks_list, key=lambda x: x.network)
+        zone_command = f"show interface {interface} | match Zone:"
+        zone_output = send_commands_to_pa(pa_connection, zone_command, string=r"virtual system:", wait=0.2)
+        zone = zone_output.split()[1][:-1]
+        net.interface = interface
+        net.zone = zone
 
-for net in sorted_pa_networks_list:
-    print(net)
+    print()
+    disconnect_from_pa(pa_connection)
+    disconnect_from_jump_server(jump_connection)
 
-sorted_bcn_networks_list = sorted(bcn_networks_list, key=lambda x: x.network)
-
-for net in sorted_bcn_networks_list:
-    print(f"{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone}")
-
-sorted_sap_networks_list = sorted(sap_networks_list, key=lambda x: x.network)
-
-for net in sorted_sap_networks_list:
-    print(f'{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone}')
 
 print()
 print('#' * 50, end='\n')
 
-disconnect_from_pa(pa_connection)
-disconnect_from_jump_server(jump_connection)
+if pa_networks_list:
+    sorted_pa_networks_list = sorted(pa_networks_list, key=lambda x: x.network)
+
+    for net in sorted_pa_networks_list:
+        print(net)
+
+if bcn_networks_list:
+    sorted_bcn_networks_list = sorted(bcn_networks_list, key=lambda x: x.network)
+
+    for net in sorted_bcn_networks_list:
+        print(f"{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone}")
+
+if sap_networks_list:
+    sorted_sap_networks_list = sorted(sap_networks_list, key=lambda x: x.network)
+
+    for net in sorted_sap_networks_list:
+        print(f'{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone}')
+
+if vsx_firewall_list:
+    sorted_vsx_firewall_list = sorted(vsx_firewall_list, key=lambda x: x.network)
+    for net in sorted_vsx_firewall_list:
+        print(f"{net.ip}/{net.host_mask} - {net.network}/{net_mask} - {net.zone} - {net.interface}")
+
+
+print()
+print('#' * 50, end='\n')
+
+
+
